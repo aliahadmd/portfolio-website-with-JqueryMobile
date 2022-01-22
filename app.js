ec7958a6a4d3cd6a5f91382b4f3d0aa9c853c04b
[Ahad Ali] <[mdaliahad@outlook.com]>
2022-01-23 00:02:06 +0800

    just upload

---
+++ app.js
@@ -0,0 +1,17 @@
+$(function () {
+    $("[data-role='footer']").toolbar();
+
+
+
+
+
+
+// social media icon style.
+    const trigger = document.querySelector("menu > .trigger");
+    trigger.addEventListener('click', (e) => {
+      e.currentTarget.parentElement.classList.toggle("open");
+    });
+
+
+    
+})
